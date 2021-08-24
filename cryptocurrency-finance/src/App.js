c2fa564fc932c8672fd9758eb106aca6f3909e73
matintynn <matintyson0402@gmail.com>
2021-08-23 22:33:57 -0400

    edit project-7 to cryptocurrency-finance

---
+++ cryptocurrency-finance/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>cryptocurrency-finance tracker</h1>
+    </div>
+  );
+}
+
+export default App;
